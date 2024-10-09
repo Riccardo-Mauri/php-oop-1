a3d1ed9764f912b8cebda42c7b8a06374ba00eac
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-10-10 00:11:03 +0200

    migliorata la disposizione/ create più funzioni di ottenimento info

--- index.php
+++ index.php
@@ -20,10 +20,22 @@ class Movie {
     }
 
 
-//Funzione per ottenere le informazioni del film
-    public function getMovieInfo(){
+//Funzione per ottenere le informazioni del film / le scorporo per provare 
+    public function getMovieTitle(){
         return
-       ''.$this->title.' '. $this->director.' '.$this->year.' '.$this->genre;
+        $this->title;
+    }
+    public function getMovieDirector(){
+        return
+       $this->director;
+    }
+    public function getMovieYear(){
+        return
+       $this->year;
+    }
+    public function getMovieGenre(){
+        return
+       $this->genre;
     }
 }
 //Defunisco quindi le mie varibili da riempire 
@@ -31,6 +43,6 @@ $Movie1 = new Movie('Inception', 'Christopher Nolan', 2010, 'thriller');
 $Movie2 = new Movie('Una Tomba per le Lucciole', 'Isao Takahata', 1988, 'drammatico');
 
 //stampo a schermo il risultato
-echo '<div>' . $Movie1 ->getMovieInfo() . '</div>';
-echo '<div>' . $Movie2 ->getMovieInfo() . '</div>';
+echo '<div>' . $Movie1 ->getMovieTitle() . '</div>' . '<div>' . $Movie1 ->getMovieDirector() .'</div>' . '<div>' . $Movie1 ->getMovieYear() . '</div>' . '<div>' . $Movie1 ->getMovieGenre() . '</div>' . '<br>';
+echo '<div>' . $Movie2 ->getMovieTitle() . '</div>' . '<div>' . $Movie2 ->getMovieDirector() .'</div>' . '<div>' . $Movie2 ->getMovieYear() . '</div>' . '<div>' . $Movie2 ->getMovieGenre() . '</div>';
 ?>
